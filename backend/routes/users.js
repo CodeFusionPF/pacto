57fe33118e052be6aabff0fcabc4a92cddcbcd98
Mauricio Giaconia <mauriciogiaco@gmail.com>
2023-05-17 16:22:34 -0300

    Added get rating of an user, added total orders in /sales route and the orders are order by date

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -15,6 +15,7 @@ const {editUser} = require('../controllers/user/APIEditUser');
 const {getUserById} = require('../controllers/user/APIgetUserById');
 const {logicBanUser} = require('../controllers/user/APIlogicBanUser');
 const { getUserByProdId } = require('../controllers/user/APIgetSellerById');
+const {getUserRating} = require('../controllers/user/APIGetUserRating');
 // const { RequestCookiesAdapter } = require('next/dist/server/web/spec-extension/adapters/request-cookies');
 
 const checkRegister = async (req, res, next) =>{
@@ -31,6 +32,9 @@ router.get('/users', checkIsAdmin,(req, res) => {getAllUsers(req, res)});
 
 //GET user by ID
 router.get('/user', (req, res) => {getUserById(req, res)});
+
+//GET user rating by ID
+router.get('/rating', (req, res) => {getUserRating(req, res)});
 
 //GET seller data by product ID
 router.get('/seller', (req, res) => {getUserByProdId(req, res)});
